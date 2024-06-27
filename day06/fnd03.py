4103049062099f6fdb3c017e8b075665e2e4c7f8
LEUNSU <lesu002@naver.com>
2024-06-27 13:42:59 +0900

    20240627

--- day06/fnd03.py
+++ day06/fnd03.py
@@ -16,22 +16,29 @@ for fndSel in fndSels:
 	GPIO.setup(fndSel, GPIO.OUT)
 	GPIO.output(fndSel, 1)
 
-def fndOut(data):
+def fndOut(data, sel): 	# 하나의 숫자 형태를 만드는 함수
 	for i in range(0, 7):
-#		GPIO.output(fndSegs[0], 0)
-#		GPIO.output(fndSegs[1], 1)
-#		GPIO.output(fndSegs[2], 1)
-#		GPIO.output(fndSegs[3], 0)
 		GPIO.output(fndSegs[i], fndData[data] & (0x01 << i))
+		for j in range(0, 2):	# 표시할 자리수의 fnd만 on
+			if j == sel:
+				GPIO.output(fndSels[j], 0)
+			else :
+				GPIO.output(fndSels[j], 1)
+
+count = 0
 
 try:
 	while True:
-		for i in range(0, 1):
-			GPIO.output(fndSels[i], 0) # fnd 선택
+		count += 1
+		d1000 = count / 1000
+		d100 = count % 1000 / 100
+		d10 = count % 100 / 10
+		d1 = count % 10
+		d = [d1, d10, d100, d1000]
 
-			for j in range(0, 10):
-				fndOut(j)
-				time.sleep(0.5)
+		for i in range(3, -1, -1):
+			fndOut(int(d[i]), i) # 자리 수와 값을 전달
+			time.sleep(0.5)
 
 except KeyboardInterrupt:
 	GPIO.cleanup()
