3962b7b95d70cce9554222688a1e3ce7e9939c00
LEUNSU <lesu002@naver.com>
2024-06-28 15:02:13 +0900

    20240628

--- day06/qt.py
+++ day06/qt.py
@@ -1,13 +1,20 @@
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from picamera2 import PiCamera
 import sys
 import RPi.GPIO as GPIO
 import time
 
-led_pin = 21
+red_pin = 21
+blue_pin = 20
+
+piezoPin = 26
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(led_pin, GPIO.OUT)
+GPIO.setup(red_pin, GPIO.OUT)
+GPIO.setup(piezoPin, GPIO.OUT)
+
+Buzz = GPIO.PWM(piezoPin, 440)
 
 form_class = uic.loadUiType("./qt.ui") [0]
 
@@ -16,23 +23,40 @@ class WindowClass(QMainWindow, form_class):
                 super().__init__()
                 self.setupUi(self)
 
+		#picam2 = Picamera2()
+
+		#LED
                 self.Btn_1.clicked.connect(self.btn01)
                 self.Btn_2.clicked.connect(self.btn02)
 
+		#CAM
+
+
+		#ALARM
+		self.Btn_5.clicked.connect(self.btn05)
+		self.Btn_6.clicked.connect(self.btn06)
 
 	#LED
         def btn01(self):
-                GPIO.output(led_pin, False)
-                print("LED ON Button Clicked")
+                GPIO.output(red_pin, False)
+                print("LED ON")
         def btn02(self):
-                GPIO.output(led_pin, True)
-                print("LED OFF Button Clicked")
+                GPIO.output(red_pin, True)
+                print("LED OFF")
 
 	#CAM
-	
+	#def btn03(self):
 
+	#def btn04(self):
 
-
+	#ALARM
+	def btn05(self):
+		Buzz.start(50)
+		print("Alarm ON")
+	def btn06(self):
+		Buzz.stop
+		GPIO.output(blue_pin, False)
+		print("Alarm OFF")
 
 def closeEvent(self, event):
         GPIO.cleanup()
