997c8b61bb29d6ec68aa6552f895ac2a71cd329f
LEUNSU <lesu002@naver.com>
2024-06-28 13:54:34 +0900

    20240628

--- day06/qt.py
+++ day06/qt.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import sys
-import RPi.GPIO as GPI
+import RPi.GPIO as GPIO
 import time
 
 led_pin = 21
@@ -16,13 +16,20 @@ class WindowClass(QMainWindow, form_class):
                 super().__init__()
                 self.setupUi(self)
 
+	self.btn01.clicked(self.btn01)
+	self.btn02.clicked(self.btn02)
+
         def btn01(self):
                 GPIO.output(led_pin, False)
                 print("LED ON Button Clicked")
 
         def btn02(self):
-                GPIO.output(red_pin, True)
+                GPIO.output(led_pin, True)
                 print("LED OFF Button Clicked")
+
+def closeEvent(self, event):
+        GPIO.cleanup()
+        event.accept()
 
 if __name__ == "__main__":
         app = QApplication(sys.argv)
